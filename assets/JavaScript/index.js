89ca77e1c35601e3a8f533bd55b31ec466de9959
Nguyễn Hải Hiếu <nguyenhaihieu89@gmail.com>
2024-10-02 19:54:49 +0700

    tick all in table feature

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -25,9 +25,26 @@ function initBack() {
     }
 }
 
+function tickAll() {
+    let checkboxALL = document.getElementsByClassName('checkboxALL');// lấy tất cả các checkboxALL
+
+    let parent = checkboxALL[0].parentNode.parentNode.parentNode;// lấy thẻ cha của checkboxALL
+
+    // lặp qua tất cả các checkboxALL
+    for (let i = 0; i < checkboxALL.length; i++) {
+        checkboxALL[i].addEventListener('click', function () {
+            let checkbox = parent.getElementsByTagName('input'); // lấy tất cả các checkbox trong thẻ cha của checkboxALL
+            for (let j = 0; j < checkbox.length; j++) {
+                checkbox[j].checked = checkboxALL[i].checked; // check hoặc uncheck tất cả các checkbox
+            }
+        });
+    }
+}
+
 function init() {
     initPopup();
     initBack();
+    tickAll();
 }
 
 init();
