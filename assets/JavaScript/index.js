71011a3482291f54b172fdef1c73d9457db84cc4
Nguyễn Hải Hiếu <nguyenhaihieu89@gmail.com>
2024-10-04 22:17:43 +0700

    add navbar and footer into index.html

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -1,30 +1,4 @@
 "use strict";
-function initPopup() {
-    let popup = document.getElementsByClassName('pop-up-element');
-    for (let i = 0; i < popup.length; i++) {
-        popup[i].addEventListener('click', function () {
-            console.log('Phần tử được click là class thứ: ' + i);
-            popup[i].nextElementSibling.style.display = (popup[i].nextElementSibling.style.display === 'none') ? 'flex' : 'none';
-            // tắt tất cả các popup khác
-            for (let j = 0; j < popup.length; j++) {
-                if (j !== i) {
-                    popup[j].nextElementSibling.style.display = 'none';
-                }
-            }
-        });
-    }
-}
-
-function initBack() {
-    let back = document.getElementsByClassName('back-element');
-    for (let i = 0; i < back.length; i++) {
-        back[i].addEventListener('click', function () {
-            console.log('Phần tử được click là class thứ: ' + i);
-            back[i].parentElement.style.display = 'none';
-        });
-    }
-}
-
 function tickAll() {
     let checkboxALL = document.getElementsByClassName('checkboxALL');// lấy tất cả các checkboxALL
 
@@ -41,12 +15,35 @@ function tickAll() {
     }
 }
 
-function init() {
-    if (window.innerWidth < 640) {
-        initPopup();
-        initBack();
-        tickAll();
-    }
+
+function initHeader() {
+    fetch('./assets/components/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+
+            const script = document.createElement("script");
+            script.src = "./assets/JavaScript/Navbar.js";
+            document.body.appendChild(script);
+        })
+        .catch(error => console.error("Lỗi tải navbar:", error));
 }
 
-init();
+function initFooter() {
+    fetch('./assets/components/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => console.error("Lỗi tải footer:", error));
+}
+
+function init() {
+    console.log('index.js loaded');
+    initHeader();
+    tickAll();
+    initFooter();
+}
+document.addEventListener("DOMContentLoaded", function () {
+    init();
+});
