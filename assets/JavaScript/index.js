f1dec0119952974314ce60196a1903e43ad5a1b4
Nguyễn Hải Hiếu <nguyenhaihieu89@gmail.com>
2024-10-09 17:10:05 +0700

    update side bar

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -1,4 +1,6 @@
 "use strict";
+
+//tạo sự kiện click cho checkboxALL
 function tickAll() {
     let checkboxALL = document.getElementsByClassName('checkboxALL');// lấy tất cả các checkboxALL
 
@@ -16,19 +18,21 @@ function tickAll() {
 }
 
 
+//tạo header
 function initHeader() {
     fetch('./assets/components/navbar.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
 
-            const script = document.createElement("script");
-            script.src = "./assets/JavaScript/Navbar.js";
-            document.body.appendChild(script);
+            const script = document.createElement("script"); // tạo thẻ script cho navbar
+            script.src = "./assets/JavaScript/Navbar.js"; // đường dẫn đến file Navbar.js
+            document.body.appendChild(script); // thêm script vào body
         })
         .catch(error => console.error("Lỗi tải navbar:", error));
 }
 
+//tạo footer
 function initFooter() {
     fetch('./assets/components/footer.html')
         .then(response => response.text())
@@ -38,12 +42,26 @@ function initFooter() {
         .catch(error => console.error("Lỗi tải footer:", error));
 }
 
+//tạo sidebar
+function initSidebar() {
+    fetch('./assets/components/sidebar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('sidebar').innerHTML = data;
+        })
+        .catch(error => console.error("Lỗi tải sidebar:", error));
+}
+
+//khởi tạo
 function init() {
     console.log('index.js loaded');
     initHeader();
     tickAll();
     initFooter();
+    initSidebar();
 }
+
+//sự kiện DOMContentLoaded xảy ra khi toàn bộ HTML đã được load xong
 document.addEventListener("DOMContentLoaded", function () {
     init();
 });
