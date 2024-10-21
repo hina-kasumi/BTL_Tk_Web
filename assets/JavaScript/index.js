63bef5ef938f0006333fddc398309b3fd5b1e1bc
Nguyễn Hải Hiếu <nguyenhaihieu89@gmail.com>
2024-10-21 13:44:23 +0700

    update current page

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -1,11 +1,7 @@
 //tạo sidebar
-function initSidebar() {
-    fetch('./assets/components/sidebar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('sidebar').innerHTML = data;
-        })
-        .catch(error => console.error("Lỗi tải sidebar:", error));
+async function initSidebar() {
+    const response = await fetch('./assets/components/sidebar.html');
+    document.getElementById('sidebar').innerHTML = await response.text();
 }
 
 //tạo footer
@@ -13,13 +9,15 @@ function initFooter() {
     fetch('./assets/components/footer.html')
         .then(response => response.text())
         .then(data => {
+            console.log("1")
             document.getElementById('footer').innerHTML = data;
         })
         .catch(error => console.error("Lỗi tải footer:", error));
+    console.log("2");
 }
 
 //tạo main content
-function initMain (filename){
+function initMain(filename) {
     fetch('./' + filename)
         .then(response => response.text())
         .then(data => {
@@ -28,13 +26,26 @@ function initMain (filename){
         .catch(error => console.log("Lỗi tải main", error));
 }
 
-function currentPage (activePage) {
-
+function currentPage() {
+    const currentUrl = window.location.href.split('/');
+    const current = currentUrl[currentUrl.length - 1].split('.')[0];
+    console.log(current);
+    document.querySelector('.current-page').classList.remove('current-page');
+    console.log(document.getElementsByClassName(current)[0])
+   document.getElementsByClassName(current)[0].classList.add('current-page');
 }
 
-function init(){
-    initSidebar()
-    initFooter();
+async function init() {
+    await initSidebar();
+    await initFooter();
+    await currentPage();
 }
+window.addEventListener('DOMContentLoaded', init);
 
-init();
+
+function tickAll() {
+    const checkBox = document.querySelectorAll('input[type="checkbox"]');
+    for (const checkBoxKey in checkBox) {
+        checkBox[checkBoxKey].checked = checkBox[0].checked;
+    }
+}
